3e2d29bb013d486b2cfac6e42c2827b1b5e865b8
Cecilia <cecilianaveira.daw@gmail.com>
2025-09-21 21:10:11 +0200

    Inicializa back

---
+++ src/main/java/com/clubesycarreraspopulares/clubescarreras/ClubescarrerasApplication.java
@@ -0,0 +1,13 @@
+package com.clubesycarreraspopulares.clubescarreras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubescarrerasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubescarrerasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clubesycarreraspopulares/clubescarreras/ClubescarrerasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clubesycarreraspopulares.clubescarreras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubescarrerasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
